d6c698905916298087e5d8cddfa9632e20e2d747
Tom Sampic <tom.sampic@insa-lyon.fr>
2020-01-18 20:38:23 +0100

    refactoring code correctly

--- projet3TC.go
+++ projet3TC.go
@@ -2,15 +2,12 @@ package main
 
 import (
 	"fmt"
-	"github.com/oliamb/cutter"
 	"github.com/wo0lien/projet3TC/filters/edge"
 	"github.com/wo0lien/projet3TC/filters/grayscale"
+	"github.com/wo0lien/projet3TC/imageTools/cutter"
 	"image"
-	"image/png"
 	"log"
-	"math"
 	"os"
-	"runtime"
 )
 
 /*
@@ -22,7 +19,6 @@ func main() {
 	//blanc variables only to avoid import errors
 	var _ = fmt.Printf
 	var _ = edge.FSobel
-	var _ = cutter.Crop
 
 	filename := "epice.png"
 	infile, err := os.Open(filename)
@@ -40,66 +36,9 @@ func main() {
 
 	// grayscale.GrayFilter(imgSrc)
 
-	slices := concurrentFilter(imgSrc)
+	slices := cutter.Cut(imgSrc)
 
 	for _, slice := range slices {
 		grayscale.GrayFilter(slice)
 	}
-}
-
-func concurrentFilter(img image.Image) []image.Image {
-
-	//traitements sur l'image
-	bounds := img.Bounds()
-	w, h := bounds.Max.X, bounds.Max.Y
-
-	var numCPU = runtime.NumCPU()
-	slice := int(math.Floor(float64(h) / float64(numCPU)))
-
-	slices := make([]image.Image, numCPU+1)
-
-	cpt := 0
-	for y := 0; y < h; y = y + slice {
-
-		ymax := min(y+slice, h)
-		// create an image copy of the slice
-		imgSliced, err := cutter.Crop(img, cutter.Config{
-			Width:   w,
-			Height:  ymax - y,
-			Anchor:  image.Point{0, y},
-			Options: cutter.Copy,
-		})
-
-		if err != nil {
-			log.Printf("Error while slicing the image")
-			panic(err.Error())
-		}
-		//z := strconv.Itoa(y)
-		//name := "slice" + z
-		//exportImage(imgSliced, name)
-		slices[cpt] = imgSliced
-		cpt++
-	}
-
-	return slices
-}
-
-func min(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
-}
-
-func exportImage(img image.Image, name string) {
-	// Encode the grayscale image to the new file
-	newFileName := name
-	newfile, err := os.Create(newFileName)
-
-	if err != nil {
-		log.Printf("failed creating png output: %s", err)
-		panic(err.Error())
-	}
-	defer newfile.Close()
-	png.Encode(newfile, img)
 }
