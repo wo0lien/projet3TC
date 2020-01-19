9ed8c5f473a64fab2be4d2a0ce2b68f0ff687c87
Tom Sampic <tom.sampic@insa-lyon.fr>
2020-01-19 22:24:48 +0100

    update edge computed image rectangle x and y to fit with conventions

--- filters/edge/edge.go
+++ filters/edge/edge.go
@@ -32,7 +32,7 @@ func FSobel(in image.Image) image.Image {
 	for cpt := miny; cpt < maxy; cpt++ {
 		for cpt2 := minx; cpt2 < maxx; cpt2++ {
 			red, gr, blue, _ := loadedImage.At(cpt2, cpt).RGBA()
-			gris[cpt][cpt2] = int16(0.2125*float32(red*255/65535) + 0.7154*float32(gr*255/65535) + 0.0721*float32(blue*255/65535))
+			gris[cpt-miny][cpt2-minx] = int16(0.2125*float32(red*255/65535) + 0.7154*float32(gr*255/65535) + 0.0721*float32(blue*255/65535))
 
 		}
 	}
@@ -45,11 +45,11 @@ func FSobel(in image.Image) image.Image {
 	for cpt := miny + 1; cpt < maxy-2; cpt++ {
 		for cpt2 := minx + 1; cpt2 < maxx-2; cpt2++ {
 			var gx float64
-			gx = float64(-1*gris[cpt-1][cpt2-1] + 1*gris[cpt+1][cpt2-1] + -2*gris[cpt-1][cpt2] + 2*gris[cpt+1][cpt2] - 1*gris[cpt-1][cpt2+1] + 1*gris[cpt+1][cpt2+1])
-			gy := float64(-1*gris[cpt-1][cpt2-1] - 2*gris[cpt][cpt2-1] - 1*gris[cpt+1][cpt2-1] + 1*gris[cpt-1][cpt2+1] + 2*gris[cpt][cpt2+1] + 1*gris[cpt+1][cpt2+1])
-			gradient[cpt][cpt2] = math.Sqrt(gx*gx + gy*gy)
-			if gradient[cpt][cpt2] > maxG {
-				maxG = gradient[cpt][cpt2]
+			gx = float64(-1*gris[cpt-1-miny][cpt2-1-minx] + 1*gris[cpt+1-miny][cpt2-1-minx] + -2*gris[cpt-1-miny][cpt2-minx] + 2*gris[cpt+1-miny][cpt2-minx] - 1*gris[cpt-1-miny][cpt2+1-minx] + 1*gris[cpt+1-miny][cpt2+1-minx])
+			gy := float64(-1*gris[cpt-1-miny][cpt2-1-minx] - 2*gris[cpt-miny][cpt2-1-minx] - 1*gris[cpt+1-miny][cpt2-1-minx] + 1*gris[cpt-1-miny][cpt2+1-minx] + 2*gris[cpt-miny][cpt2+1-minx] + 1*gris[cpt+1-miny][cpt2+1-minx])
+			gradient[cpt-miny][cpt2-minx] = math.Sqrt(gx*gx + gy*gy)
+			if gradient[cpt-miny][cpt2-minx] > maxG {
+				maxG = gradient[cpt-miny][cpt2-minx]
 			}
 
 		}
@@ -57,10 +57,10 @@ func FSobel(in image.Image) image.Image {
 	for cpt := miny + 1; cpt < maxy-2; cpt++ {
 		for cpt2 := minx + 1; cpt2 < maxx-2; cpt2++ {
 			var valsobel uint8
-			if gradient[cpt][cpt2] > 255 {
+			if gradient[cpt-miny][cpt2-minx] > 255 {
 				valsobel = 255
 			}
-			valsobel = uint8(gradient[cpt][cpt2] * 255 / maxG)
+			valsobel = uint8(gradient[cpt-miny][cpt2-minx] * 255 / maxG)
 			myImage.Set(cpt2, cpt, color.RGBA{valsobel, valsobel, valsobel, 255})
 		}
 	}
@@ -81,7 +81,7 @@ ConcurrentEdgeFilter Return the grayscale of an image and compute concurrently
 func ConcurrentEdgeFilter(imgSrc image.Image) image.Image {
 
 	out := make(chan portion)
-	slices := imagetools.CropChevauchement(imgSrc, 4, 2) //on laisse 2 pixels de chevauchement
+	slices := imagetools.CropChevauchement(imgSrc, 4, 5) //on laisse 5 pixels de chevauchement
 
 	for i := 0; i < 4; i++ {
 		go edgWorker(i, out, slices[i][0])
@@ -92,7 +92,7 @@ func ConcurrentEdgeFilter(imgSrc image.Image) image.Image {
 		slices[slice.id][0] = slice.img
 	}
 
-	imgEnd := imagetools.RebuildChevauchement(slices, 2)
+	imgEnd := imagetools.RebuildChevauchement(slices, 5)
 
 	return imgEnd
 
