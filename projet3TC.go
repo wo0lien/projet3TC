adb0e096bf93bc84c7ffb22911a995ad76a222f2
Tom Sampic <tom.sampic@insa-lyon.fr>
2020-01-19 01:05:02 +0100

    fix bugs test on main

--- projet3TC.go
+++ projet3TC.go
@@ -2,13 +2,10 @@ package main
 
 import (
 	"fmt"
-	"github.com/wo0lien/projet3TC/filters/edge"
-	"github.com/wo0lien/projet3TC/filters/grayscale"
-	"github.com/wo0lien/projet3TC/imagetools"
-	"image"
 	"log"
-	"os"
-	"runtime"
+
+	"github.com/wo0lien/projet3TC/filters/edge"
+	"github.com/wo0lien/projet3TC/imagetools"
 )
 
 /*
@@ -21,30 +18,29 @@ func main() {
 	var _ = fmt.Printf
 	var _ = edge.FSobel
 
-	filename := "epice.png"
-	infile, err := os.Open(filename)
+	testEdge2()
+
+}
+
+func testEdge2() {
+	imgSrc, err := imagetools.Open("epice.png")
 
 	if err != nil {
-		log.Printf("failed opening file: %s", err)
-		panic(err.Error())
-	}
-	defer infile.Close()
-
-	imgSrc, _, err := image.Decode(infile)
-	if err != nil {
-		panic(err.Error())
+		log.Printf("error loading file")
 	}
 
-	// grayscale.GrayFilter(imgSrc)
-
-	var numCPU = runtime.NumCPU()
-
-	slices := imagetools.Cut(imgSrc, numCPU)
+	slices := imagetools.Crop(imgSrc, 4)
 
 	for i := range slices {
-		grayscale.GrayFilter(slices[i][0])
+		log.Printf("one filter")
+		slices[i][0] = edge.FSobel(slices[i][0])
+		log.Printf("one export")
 	}
+	imgEnd := imagetools.Rebuild(slices)
+	imagetools.Export(imgEnd, "export.png")
+}
 
+func testEdge() {
 	img, err := imagetools.Open("assets/test.png")
 
 	if err != nil {
